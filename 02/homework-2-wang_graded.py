d22c6d950797fe6592e50f2723aa2221131a8462
Stephan Bisaha <bisahas1@gmail.com>
2016-06-07 23:07:55 -0400

    TA-STEPHAN: Added graded homework 2.

---
+++ 02/homework-2-wang_graded.py
@@ -0,0 +1,90 @@
+# Graded: 12/15
+#Zhizhou Wang
+# May 25, 2016
+# Homework 2
+
+#Make a list of the following numbers: 22, 90, 0, -10, 3, 22, and 48
+numbers = [22,90,0,-10,3,22,48]
+
+#Display the number of elements in the list
+print(numbers)
+#TA-STEPHAN: Did not display the number of elements in the list, just the numbers
+# Should use len.
+
+#Display the 4th element of this list.
+print(numbers[3])
+
+#Display the sum of the 2nd and 4th element of the list.
+print(numbers[1] + numbers[3])
+
+#Display the 2nd-largest value in the list.
+sorted_numbers=sorted(numbers,reverse=True)
+print("2nd-largest value",sorted_numbers[1])
+
+#Display the last element of the original unsorted list
+#print(len(numbers)) #7
+print(numbers[len(numbers) - 2])
+#TA-STEPHAN: Should be -1, not -2 if you use this method.
+
+# For each number, display a number:
+for i in numbers:
+    answer = i
+    if i < 10:
+        answer = answer * 30
+    if i % 2 == 0:
+            answer = answer + 6
+    if i != -10:
+                answer = answer -1
+    print(answer)
+
+#TA-STEPHAN: You're missing a few conditions here. Here's an example of the full
+#for loop.
+
+#Example:
+#for number in numbers:
+#    original = number
+#    if original > 50:
+#        number = number - 10
+#    if original < 10:
+#        number = number * 30
+#        if original % 2 == 0:
+#            number = number + 6
+#    if original > -10:
+#        number = number - 1
+#    print(number)
+
+#Sum the result of each of the numbers divided by two.
+result = 0
+for i in numbers:
+    result = result + i/2
+print (result)
+
+#Define a dictionary called movie that works with the following code
+movie = {"title":"Captain America","year":2011, "director":"Joe Johnston"}
+print("My favorite movie is", movie["title"] , "which was released in",movie["year"],"and was directed by",movie["director"])
+
+# Add entries to the movie dictionary for budget and revenue
+movie["budget"]=140
+movie["revenue"]=370
+print(abs(movie["revenue"]-movie["budget"]))
+
+# If the movie cost more to make than it made in theaters, print "It was a flop". If the film's revenue was more than five times the amount it cost to make, print "That was a good investment."
+if movie["revenue"] < movie["budget"]:
+    print("It was a flop")
+elif movie["revenue"] > (movie["budget"]*5):
+    print("That was a good investment")
+else:
+    print("So so")
+
+#Make ONE dictionary that describes the population of the boroughs of NYC
+population = {"Manhattan":1600,"Brooklyn":2600,"Bronx":1400,"Queens":2300,"Staten Island":470}
+
+#Display the population of Brooklyn.
+print("The population of Brooklyn is",population["Brooklyn"],"thousands.")
+
+#Display the combined population of all five boroughs.
+print(sum(population.values()),"thousands")
+
+#Display what percent of NYC's population lives in Manhattan.
+total = sum(population.values())
+print(str(round(population["Manhattan"]/total*100,2))+"%")
